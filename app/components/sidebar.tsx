1392d434e3533483121ec05d5c07e448554ddd4d
jhl1789 <56823343+jhl1789@users.noreply.github.com>
2023-08-21 21:43:19 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          JWT's own AI assistant.
+          JWT s own AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
